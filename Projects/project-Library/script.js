1083280cc4860437d2bcbc848e9ec110b2da8788
edwyip <clyip93@gmail.com>
2022-09-19 19:51:09 +0800

    implemented book displaying script

--- Projects/project-Library/script.js
+++ Projects/project-Library/script.js
@@ -28,3 +28,56 @@ addBookToLibrary(METAMORPHOSIS)
 addBookToLibrary(SUTTREE)
 
 const books = document.querySelector(".books")
+
+function addBook(book) {
+    const newBook = document.createElement("div")
+    newBook.classList.add("book")
+
+    //make a delete cross button
+    const del = document.createElement("div")
+    del.classList.add("delete-book")
+    const delBtn = document.createElement("button")
+    delBtn.textContent = "x"
+    del.appendChild(delBtn)
+
+    //make the info section
+    const info = document.createElement("div")
+    info.classList.add("info")
+    for (let attr in book) {
+        let newDiv = document.createElement("div")
+        newDiv.classList.add(attr)
+        switch (attr) {
+            case "pages":
+                newDiv.textContent = "Total Pages: " + book[attr]
+                break
+            case "read":
+                newDiv.textContent = (book[attr] === true) ? "Already read" : "Not read yet"
+                break
+            default:
+                newDiv.textContent = book[attr]
+        }
+        info.appendChild(newDiv)
+    }
+
+    newBook.appendChild(del)
+    newBook.appendChild(info)
+
+    const books = document.querySelector(".books")
+    books.appendChild(newBook)
+
+}
+
+function displayBooks(library) {
+    for (let book of library) {
+        addBook(book)
+    }
+}
+
+displayBooks(mylibrary)
+
+function deleteBook(book) {
+    const delBtn = document.querySelector("delete-book")
+    delBtn.addEventListener("click", () => {
+        delBtn.parentNode.setAttribute("style", "display:none")
+    })
+}
