0ae2b57157f31fbf6f5e70b449d826e2e3648264
edwyip <clyip93@gmail.com>
2022-09-28 16:12:01 +0800

    implemented Gameboard.render() functionality

--- Projects/project-TicTacToe/script.js
+++ Projects/project-TicTacToe/script.js
@@ -1,11 +1,30 @@
 const Gameboard = (function() {
+    const render = () => {
+        const container = document.querySelector(".container")
+        for (let i = 0; i < 9; i++) {
+            const square = document.createElement("div")
+            square.classList.add("square")
+            container.appendChild(square)
+        }
 
+    }
+
+    return { render }
 })()
 
-const GisplayController = (function() {
+const DisplayController = (function() {
 
 })()
 
 const Player = () => {
+    const squares = [...document.querySelectorAll("square")]
+    const play = (player) => {
+        squares.forEach((square) => {
+            square.addEventListener("click", (e) => {
+                e.textContent = player
+            })
+        })
+    }
 
+    return { play }
 }
