fe2ee478a9819a5c454213fc008d3caf0fa8568c
edwyip <clyip93@gmail.com>
2022-09-28 21:13:04 +0800

    implemented switchHand functionality

--- Projects/project-TicTacToe/script.js
+++ Projects/project-TicTacToe/script.js
@@ -1,30 +1,54 @@
 const Gameboard = (function() {
-    const render = () => {
-        const container = document.querySelector(".container")
-        for (let i = 0; i < 9; i++) {
-            const square = document.createElement("div")
-            square.classList.add("square")
-            container.appendChild(square)
-        }
+    const newBoard = () =>{
+        const newGameBtn = document.querySelector(".newGame")
+        
+        //render new board
+        newGameBtn.addEventListener("click", () => {
+            const container = document.querySelector(".container")
+            while(container.firstChild){
+                container.removeChild(container.lastChild)
+            }
+            //temp var
+            let hand = "X"
 
-    }
+            for (let i = 0; i < 9; i++) {
+                const square = document.createElement("div")
+                square.classList.add("square")
+                
+                //add click listener for marks
+                square.addEventListener("click",()=>{
+                    square.textContent = hand
+                    hand = switchHand(hand)
+                }, { once: true })
 
-    return { render }
-})()
-
-const DisplayController = (function() {
-
-})()
-
-const Player = () => {
-    const squares = [...document.querySelectorAll("square")]
-    const play = (player) => {
-        squares.forEach((square) => {
-            square.addEventListener("click", (e) => {
-                e.textContent = player
-            })
+                container.appendChild(square)
+            }
         })
     }
 
-    return { play }
-}
+    const switchHand = (hand) => {
+        hand = hand==="X"? "O":"X"
+        return hand
+    }
+
+    return { newBoard }
+})()
+
+// const Player = (hand) => {
+//     const name = prompt(`Who's playing ${hand}`?)
+
+//     return { name, play }
+// }
+
+// const DisplayController = (function() {
+//     Gameboard.newBoard()
+//     const player1 = Player("X") //"X" plays first
+//     const player2 = Player("O")
+//     let player = "X"
+//     const switchPlayer = player => {
+//         player = (player===player1)? player2: player1
+//         return player
+//     }
+// })()
+
+Gameboard.newBoard()
